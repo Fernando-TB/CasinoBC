3251d819f6ced6ed85c270f483069ee13275207f
Fernando-TB <f.torres17@ufromail.cl>
2025-11-23 23:39:26 -0300

    Test 7 fui incapaz de realizar los demas test por falta de algunos metodos

---
+++ src/test/java/modelo/UsuarioTest.java
@@ -0,0 +1,23 @@
+package modelo;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+
+    @BeforeEach
+
+    @Test
+    void validarCredenciales() {
+
+        Usuario user = new Usuario("ft", "123", "fernan");
+
+        boolean res = user.validarCredenciales(null, "123");
+
+        assertFalse(res);
+    }
+
+}
